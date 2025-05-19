b0bc9e3025c92bbe6af7692e560208546420ecc2
Constellation-Analytics <paul.j.chapman@outlook.com>
2025-05-19 19:33:09 +0200

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -1,3 +1,6 @@
 
 -- stk_personal
-CREATE TABLE IF NOT EXISTS stk_personal(date date, code varchar, close int, ema30 int ema60 int, ema180 int);
+CREATE TABLE IF NOT EXISTS stk_personal(date date, code varchar, close int, ema30 int, ema60 int, ema180 int);
+
+-- stk_market
+CREATE TABLE IF NOT EXISTS stk_market(date date, code varchar, close int, ema30 int, ema60 int, ema180 int);
