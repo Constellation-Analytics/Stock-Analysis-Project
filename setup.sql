7e72d0f0f78ba0455ec250f403a4922f276487c3
Constellation-Analytics <paul.j.chapman@outlook.com>
2025-05-20 13:14:49 +0200

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -1,7 +1,10 @@
 
 -- stk_personal
 CREATE TABLE IF NOT EXISTS stk_personal(date date, code varchar, close int, ema30 int, ema60 int, ema180 int, date_entered date);
-
+ALTER TABLE "public"."stk_personal"
+ADD CONSTRAINT "pk_stk_personal" PRIMARY KEY ("date", "code")
+  
 -- stk_market
 CREATE TABLE IF NOT EXISTS stk_market(date date, code varchar, close int, ema30 int, ema60 int, ema180 int, date_entered date);
-
+ALTER TABLE "public"."stk_market"
+ADD CONSTRAINT "pk_stk_market" PRIMARY KEY ("date", "code")
