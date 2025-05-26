12ea69b41e87d3e7589279ed6b5e969c0206382f
Constellation-Analytics <paul.j.chapman@outlook.com>
2025-05-26 17:15:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import yfinance as yf
+import pandas as pd
 import psycopg2
 import os
 from datetime import datetime
@@ -6,34 +7,17 @@ from datetime import datetime
 # Load environment variables from GitHub Secrets
 NEON_DB = os.getenv("NEON_DB")
 
-def get_stock_data(ticker="AAPL"):
-    stock = yf.Ticker(ticker)
-    hist = stock.history(period="1d")
-    latest = hist.iloc[-1]
-    return {
-        "ticker": ticker,
-        "date": datetime.now().date(),  # <-- use current date instead
-        "close": float(latest["Close"])
-    }
-
-def insert_to_db(data):
+def insert_to_db(data, table):
+    """
+    THIS FUNCTION NEEDS TO BE UPDATED
+    """
     conn = psycopg2.connect(NEON_DB)
     cur = conn.cursor()
-    cur.execute("""
-        CREATE TABLE IF NOT EXISTS stock_data (
-            ticker TEXT,
-            date DATE,
-            close FLOAT
-        );
     """)
     cur.execute(
-        "INSERT INTO stock_data (ticker, date, close) VALUES (%s, %s, %s);",
+        f"INSERT INTO {table} (ticker, date, close) VALUES (%s, %s, %s);",
         (data["ticker"], data["date"], data["close"])
     )
     conn.commit()
     cur.close()
     conn.close()
-
-if __name__ == "__main__":
-    data = get_stock_data("AAPL")
-    insert_to_db(data)
